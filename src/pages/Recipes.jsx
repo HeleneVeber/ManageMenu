c80752065c40e95667bf18ba9233d028ac3beb25
HeleneVeber <veber.helene@gmail.com>
2023-05-21 15:14:30 +0200

    récupération du fetch dans recipes

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from "react";
+import { getRecipes } from "../api/getRecipes";
 import Header from "../components/Header/Header";
 import Checkbox from "../components/Utils/Checkbox";
-import getRecipes from "../../src/api/getRecipes";
-
 //import { styled } from "styled-components";
 
 export default function Recipes() {
-  const [recipesChecked, setRecipesChecked] = useState([]);
-  const [recipesData, setrecipesData] = useState([
+  const [recipesChecked, setRecipesChecked] = useState([
     {
       recipe: "",
       checked: false,
@@ -18,6 +16,7 @@ export default function Recipes() {
   useEffect(() => {
     const getRecipesLoad = async () => {
       const data = await getRecipes();
+
       const recipes = data.map((e) => {
         return {
           recipe: e.recipe.label,
@@ -25,59 +24,34 @@ export default function Recipes() {
           ingredients: e.recipe.ingredientLines,
         };
       });
-      setrecipesData(recipes);
+      setRecipesChecked(recipes);
     };
     getRecipesLoad();
   }, []);
 
-  useEffect(() => {
-    setRecipesChecked(recipesData);
-  }, [recipesData]);
-
   const handleRecipesChecked = (index) => {
-    const newRecipesChecked = [...recipesChecked];
-    newRecipesChecked[index].checked = !newRecipesChecked[index].checked;
-    setRecipesChecked(newRecipesChecked);
+    const updateRecipesChecked = [...recipesChecked];
+    updateRecipesChecked[index].checked = !updateRecipesChecked[index].checked;
+    setRecipesChecked(updateRecipesChecked);
   };
-
-  // const getIngredients = (array) => {
-  //   const ingredients = [];
-  //   array.map((e) => {
-  //     if (e.checked) {
-  //       ingredients.push(e.ingredients);
-  //       console.log("ingredients", ingredients);
-  //     }
-  //   });
-  //   return ingredients;
-  // };
-  // const toto = getIngredients(recipeData);
-  // console.log("toto", toto);
-
-  // const test = { message: "bonjour" };
-  //getIngredients(recipeData);
-
-  console.log("recipesData", recipesData);
-  console.log("recipesChecked", recipesChecked);
+  //console.log(recipesChecked);
   return (
     <>
       <Header />
       <main>
         <h2>Choisissez vos recettes</h2>
-        <form action="/ShoppingList">
-          {recipesData.map((e, i) => (
-            <div key={i}>
-              <Checkbox
-                index={i}
-                label={e.recipe}
-                checked={e.checked}
-                nameId={`recipe-${i}`}
-                onChange={() => handleRecipesChecked(i)}
-              />
-            </div>
-          ))}
 
-          <input type="submit" value="Validez" />
-        </form>
+        {recipesChecked.map((e, i) => (
+          <div key={i}>
+            <Checkbox
+              index={i}
+              label={e.recipe}
+              checked={e.checked}
+              nameId={`recipe-${i}`}
+              onChange={() => handleRecipesChecked(i)}
+            />
+          </div>
+        ))}
       </main>
     </>
   );
