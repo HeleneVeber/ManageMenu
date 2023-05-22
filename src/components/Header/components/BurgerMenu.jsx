48b8626f7933d86667b3768fac50b83c0226d2bd
HeleneVeber <veber.helene@gmail.com>
2023-05-22 22:31:30 +0200

    reconfig eslint/prettier

--- src/components/Header/components/BurgerMenu.jsx
+++ src/components/Header/components/BurgerMenu.jsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { styled } from "styled-components";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-//TODO: Refactoring and style
+// TODO: Refactoring and style
 
 export default function BurgerMenu() {
-  const [showNavBar, setShowNavBar] = useState(false);
+    const [showNavBar, setShowNavBar] = useState(false);
 
-  const handleNavBar = () => {
-    setShowNavBar(!showNavBar);
-  };
+    const handleNavBar = () => {
+        setShowNavBar(!showNavBar);
+    };
 
-  return (
-    <>
-      <MenuButton onClick={handleNavBar}>
-        <img src="../../../../src/assets/menu.png" alt="Menu" />
-      </MenuButton>
-      <nav style={{ display: showNavBar ? "inline" : "none" }}>
-        <PageLink to="/recipes">Vos recettes</PageLink>
-        <PageLink to="/shoppingList">Votre liste de course</PageLink>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <MenuButton onClick={handleNavBar}>
+                <img src="../../../../src/assets/menu.png" alt="Menu" />
+            </MenuButton>
+            <nav style={{ display: showNavBar ? 'inline' : 'none' }}>
+                <PageLink to="/recipes">Vos recettes</PageLink>
+                <PageLink to="/shoppingList">Votre liste de course</PageLink>
+            </nav>
+        </>
+    );
 }
 
 const PageLink = styled(Link)`
-  font-family: "IBM Plex Sans Thai Looped";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 33px;
-  display: flex;
-  align-items: center;
-  color: #403f3e;
+    font-family: 'IBM Plex Sans Thai Looped';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 33px;
+    display: flex;
+    align-items: center;
+    color: #403f3e;
 `;
 
 const MenuButton = styled.button`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  background-color: inherit;
-  color: #fff;
-  border: none;
-  cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background-color: inherit;
+    color: #fff;
+    border: none;
+    cursor: pointer;
 `;
