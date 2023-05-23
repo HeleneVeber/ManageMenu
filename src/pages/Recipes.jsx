962abeae2d79a1d0a5476f40950ce441d1e01cc3
HeleneVeber <veber.helene@gmail.com>
2023-05-23 09:07:26 +0200

    reconfig eslint/prettier

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,41 +1,41 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import Header from "../components/Header/Header";
-import Checkbox from "../components/Utils/Checkbox";
-//import { styled } from "styled-components";
+import { Link } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import Checkbox from '../components/Utils/Checkbox';
+// import { styled } from "styled-components";
 
 export default function Recipes({ recipes, setRecipes }) {
-  const handleRecipesChecked = (index) => {
-    const newRecipes = [...recipes];
-    newRecipes[index].checked = !newRecipes[index].checked;
-    setRecipes(newRecipes);
-  };
+    const handleRecipesChecked = (index) => {
+        const newRecipes = [...recipes];
+        newRecipes[index].checked = !newRecipes[index].checked;
+        setRecipes(newRecipes);
+    };
 
-  console.log("recipes", recipes);
-  return (
-    <>
-      <Header />
-      <main>
-        <h2>Choisissez vos recettes</h2>
+    // console.log('recipes', recipes);
+    return (
+        <>
+            <Header />
+            <main>
+                <h2>Choisissez vos recettes</h2>
 
-        <form>
-          {recipes.map((e, i) => (
-            <div key={i}>
-              <Checkbox
-                index={i}
-                label={e.recipe}
-                checked={e.checked}
-                nameId={`recipe-${i}`}
-                onChange={() => handleRecipesChecked(i)}
-              />
-            </div>
-          ))}
+                <form>
+                    {recipes.map((e) => (
+                        <div key={e.id}>
+                            <Checkbox
+                                index={e.id}
+                                label={e.recipe}
+                                checked={e.checked}
+                                nameId={`recipe-${e.id}`}
+                                onChange={() => handleRecipesChecked(e.id)}
+                            />
+                        </div>
+                    ))}
 
-          <Link to="/shoppingList">
-            <input type="submit" value="Validez" />
-          </Link>
-        </form>
-      </main>
-    </>
-  );
+                    <Link to="/shoppingList">
+                        <input type="submit" value="Validez" />
+                    </Link>
+                </form>
+            </main>
+        </>
+    );
 }
